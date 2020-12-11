904b8a16c8b5f467ccc28a429bf9e3ad2717133b
AINIJOS <56822531+AINIJOS@users.noreply.github.com>
2020-12-12 01:40:29 +0600

    Update selectBasics.sql

--- selectBasics.sql
+++ selectBasics.sql
@@ -1,5 +1,4 @@
 -- 1 The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
-
 -- Modify it to show the population of Germany
 
 SELECT population FROM world
@@ -8,7 +7,6 @@ SELECT population FROM world
 
 --   2.
 -- Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
-
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 
 SELECT name, population FROM world
